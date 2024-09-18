1305549d5561e5e750f8a9596d61454d730df3ec
sergey <s.petykov@flat-soft.ru>
2024-09-18 10:31:05 +0300

    added ghToken

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/annotation/Spending.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/annotation/Spending.java
@@ -1,4 +1,4 @@
-package guru.qa.niffler.jupiter;
+package guru.qa.niffler.jupiter.annotation;
 
 import guru.qa.niffler.jupiter.extension.CreateSpendingExtension;
 import guru.qa.niffler.jupiter.extension.SpendingResolverExtension;
--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/extension/CategoryExtension.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/extension/CategoryExtension.java
@@ -2,7 +2,7 @@ package guru.qa.niffler.jupiter.extension;
 
 import com.github.javafaker.Faker;
 import guru.qa.niffler.api.SpendApiClient;
-import guru.qa.niffler.jupiter.Category;
+import guru.qa.niffler.jupiter.annotation.Category;
 import guru.qa.niffler.model.CategoryJson;
 import org.junit.jupiter.api.extension.*;
 import org.junit.platform.commons.support.AnnotationSupport;
