7f93614073cf7d0158b44470256a79f8ea047303
sergey <s.petykov@flat-soft.ru>
2024-09-25 16:15:08 +0300

    in progress

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/data/dao/impl/UserdataUserDAOJdbc.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/data/dao/impl/UserdataUserDAOJdbc.java
@@ -3,8 +3,6 @@ package guru.qa.niffler.data.dao.impl;
 import guru.qa.niffler.config.Config;
 import guru.qa.niffler.data.Databases;
 import guru.qa.niffler.data.dao.UserdataUserDAO;
-import guru.qa.niffler.data.entity.spend.CategoryEntity;
-import guru.qa.niffler.data.entity.spend.SpendEntity;
 import guru.qa.niffler.data.entity.user.UserEntity;
 import guru.qa.niffler.model.CurrencyValues;
 
