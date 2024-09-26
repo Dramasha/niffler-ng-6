03fb6a6fcd36c66f9ed367947e5a249c4d26dd6b
sergey <s.petykov@flat-soft.ru>
2024-09-26 17:03:18 +0300

    in progress

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/data/Databases.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/data/Databases.java
@@ -1,34 +1,174 @@
 package guru.qa.niffler.data;
 
+import com.atomikos.icatch.jta.UserTransactionImp;
+import com.atomikos.jdbc.AtomikosDataSourceBean;
+import jakarta.transaction.SystemException;
+import jakarta.transaction.UserTransaction;
+import org.apache.commons.lang3.StringUtils;
 import org.postgresql.ds.PGSimpleDataSource;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class Databases {
     private Databases() {
     }
 
     private static final Map<String, DataSource> dataSources = new ConcurrentHashMap<>();
+    private static final Map<Long, Map<String, Connection>> threadConnections = new ConcurrentHashMap<>();
 
-    private static DataSource getDataSource(String jdbcUrl) {
-        return dataSources.computeIfAbsent(
-                jdbcUrl,
-                key -> {
-                    PGSimpleDataSource ds = new PGSimpleDataSource();
-                    ds.setUser("postgres");
-                    ds.setPassword("secret");
-                    ds.setUrl(key);
-                    return ds;
+    public record XaFunction<T>(Function<Connection, T> function, String jdbcUrl) {}
+
+
+    public record XaConsumer(Consumer<Connection> consumer, String jdbcUrl) {}
+
+
+    public static <T> T transaction(Function<Connection, T> function, String jdbcUrl) {
+        Connection connection = null;
+        try {
+            connection = connection(jdbcUrl);
+            connection.setAutoCommit(false);
+            T result = function.apply(connection);
+            connection.commit();
+            connection.setAutoCommit(true);
+            return result;
+        } catch (SQLException e) {
+            if (connection != null) {
+                try {
+                    connection.rollback();
+                    connection.setAutoCommit(true);
+                } catch (SQLException ex) {
+                    throw new RuntimeException(ex);
                 }
-        );
+            }
+            throw new RuntimeException(e);
+        }
     }
 
-    public static Connection connection(String jdbcUrl) throws SQLException {
-        return getDataSource(jdbcUrl).getConnection();
+    public static <T> T xaTransaction(XaFunction<T>... actions) {
+        UserTransaction ut = new UserTransactionImp();
+        try {
+            ut.begin();
+            T result = null;
+            for (XaFunction<T> action : actions) {
+                result = action.function.apply(connection(action.jdbcUrl));
+            }
+            ut.commit();
+            return result;
+        } catch (Exception e) {
+            try {
+                ut.rollback();
+
+            } catch (SystemException ex) {
+                throw new RuntimeException(ex);
+            }
+            throw new RuntimeException(e);
+        }
     }
 
-}
+    public static void transaction(Consumer<Connection> consumer, String jdbcUrl) {
+        Connection connection = null;
+        try {
+            connection = connection(jdbcUrl);
+            connection.setAutoCommit(false);
+            consumer.accept(connection);
+            connection.commit();
+            connection.setAutoCommit(true);
+        } catch (SQLException e) {
+            if (connection != null) {
+                try {
+                    connection.rollback();
+                    connection.setAutoCommit(true);
+                } catch (SQLException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void xaConsumer(XaConsumer... actions) {
+        UserTransaction ut = new UserTransactionImp();
+        try {
+            ut.begin();
+            for (XaConsumer action : actions) {
+                action.consumer.accept(connection(action.jdbcUrl));
+            }
+            ut.commit();
+        } catch (Exception e) {
+            try {
+                ut.rollback();
+
+            } catch (SystemException ex) {
+                throw new RuntimeException(ex);
+            }
+            throw new RuntimeException(e);
+        }
+    }
+
+
+        private static DataSource getDataSource (String jdbcUrl){
+            return dataSources.computeIfAbsent(
+                    jdbcUrl,
+                    key -> {
+                        AtomikosDataSourceBean dsBean = new AtomikosDataSourceBean();
+                        final String uniqId = StringUtils.substringAfter(jdbcUrl, "5432/");
+                        dsBean.setUniqueResourceName(uniqId);
+                        dsBean.setXaDataSourceClassName("org.postgresql.xa.PGXADataSource");
+                        Properties properties = new Properties();
+                        properties.put("URL", jdbcUrl);
+                        properties.put("user", "postgres");
+                        properties.put("password", "secret");
+                        dsBean.setXaProperties(properties);
+                        dsBean.setPoolSize(10);
+                        return dsBean;
+                    }
+            );
+        }
+
+        public static Connection connection (String jdbcUrl) throws SQLException {
+            return threadConnections.computeIfAbsent(
+                    Thread.currentThread().threadId(),
+                    key -> {
+                        try {
+                            return new HashMap<>(Map.of(
+                                    jdbcUrl,
+                                    getDataSource(jdbcUrl).getConnection()
+                            ));
+                        } catch (SQLException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+            ).computeIfAbsent(
+                    jdbcUrl,
+                    key -> {
+                        try {
+                            return getDataSource(jdbcUrl).getConnection();
+                        } catch (SQLException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+            );
+        }
+
+        public static void closeAllConnections () {
+            for (Map<String, Connection> connectionsMap : threadConnections.values()) {
+                for (Connection connection : connectionsMap.values()) {
+                    try {
+                        if (connection != null && !connection.isClosed()) {
+                            connection.close();
+                        }
+                    } catch (SQLException e) {
+                        //NOP
+                    }
+                }
+            }
+        }
+    }
--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/data/dao/impl/UserdataUserDAOJdbc.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/data/dao/impl/UserdataUserDAOJdbc.java
@@ -11,67 +11,68 @@ import java.util.Optional;
 import java.util.UUID;
 
 public class UserdataUserDAOJdbc implements UserdataUserDAO {
+    private final Connection connection;
+
+    public UserdataUserDAOJdbc(Connection connection) {
+        this.connection = connection;
+    }
 
     private static final Config CFG = Config.getInstance();
 
     @Override
     public UserEntity create(UserEntity user) {
-        try (Connection connection = Databases.connection(CFG.userdataJdbcUrl())) {
-            try (PreparedStatement statement = connection.prepareStatement(
-                    "INSERT INTO user (username, currency, firstname, surname, photo, photo_small, full_name) " +
-                            "VALUES (?,?,?,?,?,?,?)",
-                    Statement.RETURN_GENERATED_KEYS
-            )) {
-                statement.setString(1, user.getUsername());
-                statement.setObject(2, user.getCurrency());
-                statement.setString(3, user.getFirstname());
-                statement.setString(4, user.getSurname());
-                statement.setBytes(5, user.getPhoto());
-                statement.setBytes(6, user.getPhotoSmall());
-                statement.setString(7, user.getFullname());
+        try (PreparedStatement statement = connection.prepareStatement(
+                "INSERT INTO user (username, currency, firstname, surname, photo, photo_small, full_name) " +
+                        "VALUES (?,?,?,?,?,?,?)",
+                Statement.RETURN_GENERATED_KEYS
+        )) {
+            statement.setString(1, user.getUsername());
+            statement.setObject(2, user.getCurrency());
+            statement.setString(3, user.getFirstname());
+            statement.setString(4, user.getSurname());
+            statement.setBytes(5, user.getPhoto());
+            statement.setBytes(6, user.getPhotoSmall());
+            statement.setString(7, user.getFullname());
 
-                statement.executeUpdate();
+            statement.executeUpdate();
 
-                final UUID generatedKey;
-                try (ResultSet resultSet = statement.getGeneratedKeys()) {
-                    if (resultSet.next()) {
-                        generatedKey = resultSet.getObject("id", UUID.class);
-                    } else {
-                        throw new SQLException("Cant find id in ResultSet");
-                    }
+            final UUID generatedKey;
+            try (ResultSet resultSet = statement.getGeneratedKeys()) {
+                if (resultSet.next()) {
+                    generatedKey = resultSet.getObject("id", UUID.class);
+                } else {
+                    throw new SQLException("Cant find id in ResultSet");
                 }
-                user.setId(generatedKey);
-                return user;
             }
+            user.setId(generatedKey);
+            return user;
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
 
     public Optional<UserEntity> findUserById(UUID id) {
-        try (Connection connection = Databases.connection(CFG.userdataJdbcUrl())) {
-            try (PreparedStatement statement = connection.prepareStatement(
-                    "SELECT * FROM user WHERE id = ?"
-            )) {
-                statement.setObject(1, id);
-                statement.execute();
-                try (ResultSet resultSet = statement.getResultSet()) {
-                    if (resultSet.next()) {
-                        UserEntity user = new UserEntity();
+        try (PreparedStatement statement = connection.prepareStatement(
+                "SELECT * FROM user WHERE id = ?"
+        )) {
+            statement.setObject(1, id);
+            statement.execute();
+            try (ResultSet resultSet = statement.getResultSet()) {
+                if (resultSet.next()) {
+                    UserEntity user = new UserEntity();
 
-                        user.setId(resultSet.getObject("id", UUID.class));
-                        user.setUsername(resultSet.getString("username"));
-                        user.setCurrency(resultSet.getObject("currency", CurrencyValues.class));
-                        user.setFirstname(resultSet.getString("firstname"));
-                        user.setSurname(resultSet.getString("surname"));
-                        user.setPhoto(resultSet.getBytes("photo"));
-                        user.setPhotoSmall(resultSet.getBytes("photo_small"));
-                        user.setFullname(resultSet.getString("full_name"));
+                    user.setId(resultSet.getObject("id", UUID.class));
+                    user.setUsername(resultSet.getString("username"));
+                    user.setCurrency(resultSet.getObject("currency", CurrencyValues.class));
+                    user.setFirstname(resultSet.getString("firstname"));
+                    user.setSurname(resultSet.getString("surname"));
+                    user.setPhoto(resultSet.getBytes("photo"));
+                    user.setPhotoSmall(resultSet.getBytes("photo_small"));
+                    user.setFullname(resultSet.getString("full_name"));
 
-                        return Optional.of(user);
-                    } else {
-                        return Optional.empty();
-                    }
+                    return Optional.of(user);
+                } else {
+                    return Optional.empty();
                 }
             }
         } catch (SQLException e) {
@@ -80,29 +81,27 @@ public class UserdataUserDAOJdbc implements UserdataUserDAO {
     }
 
     public Optional<UserEntity> findUserByUsername(UserEntity username) {
-        try (Connection connection = Databases.connection(CFG.userdataJdbcUrl())) {
-            try (PreparedStatement statement = connection.prepareStatement(
-                    "SELECT * FROM user WHERE username = ?"
-            )) {
-                statement.setObject(1, username);
-                statement.execute();
-                try (ResultSet resultSet = statement.getResultSet()) {
-                    if (resultSet.next()) {
-                        UserEntity user = new UserEntity();
+        try (PreparedStatement statement = connection.prepareStatement(
+                "SELECT * FROM user WHERE username = ?"
+        )) {
+            statement.setObject(1, username);
+            statement.execute();
+            try (ResultSet resultSet = statement.getResultSet()) {
+                if (resultSet.next()) {
+                    UserEntity user = new UserEntity();
 
-                        user.setId(resultSet.getObject("id", UUID.class));
-                        user.setUsername(resultSet.getString("username"));
-                        user.setCurrency(resultSet.getObject("currency", CurrencyValues.class));
-                        user.setFirstname(resultSet.getString("firstname"));
-                        user.setSurname(resultSet.getString("surname"));
-                        user.setPhoto(resultSet.getBytes("photo"));
-                        user.setPhotoSmall(resultSet.getBytes("photo_small"));
-                        user.setFullname(resultSet.getString("full_name"));
+                    user.setId(resultSet.getObject("id", UUID.class));
+                    user.setUsername(resultSet.getString("username"));
+                    user.setCurrency(resultSet.getObject("currency", CurrencyValues.class));
+                    user.setFirstname(resultSet.getString("firstname"));
+                    user.setSurname(resultSet.getString("surname"));
+                    user.setPhoto(resultSet.getBytes("photo"));
+                    user.setPhotoSmall(resultSet.getBytes("photo_small"));
+                    user.setFullname(resultSet.getString("full_name"));
 
-                        return Optional.of(user);
-                    } else {
-                        return Optional.empty();
-                    }
+                    return Optional.of(user);
+                } else {
+                    return Optional.empty();
                 }
             }
         } catch (SQLException e) {
@@ -111,10 +110,9 @@ public class UserdataUserDAOJdbc implements UserdataUserDAO {
     }
 
     public void deleteSpendById(UserEntity user) {
-        try (Connection connection = Databases.connection(CFG.userdataJdbcUrl());
-             PreparedStatement statement = connection.prepareStatement(
-                     "DELETE FROM user WHERE id = ?"
-             )) {
+        try (PreparedStatement statement = connection.prepareStatement(
+                "DELETE FROM user WHERE id = ?"
+        )) {
             statement.setObject(1, user.getId());
             statement.executeUpdate();
         } catch (SQLException e) {
